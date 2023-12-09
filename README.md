b744e9157ff28c7410a7ad807d52e848998216bf
Tushar Ojha <tusharojha2001@gmail.com>
2023-12-10 02:19:02 +0530

    added v2 integration start

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 A Generative AI + Blockchain text-based game that enables readers to be a part of an interesting story that unveils as they take actions in the universe (called lok in sanskrit).
 
-Built on Solana. 
+Built on Storyweb Micro Rollup on Scroll, Base, Filecoin / Lighthouse Storage. 
--- components/config.ts
+++ components/config.ts
@@ -1,19 +1,19 @@
 import { WalletEntryPosition } from '@particle-network/auth'
-import { Solana } from '@particle-network/chains'
-import { solanaWallets } from '@particle-network/connect'
+import { MantleTestnet, ScrollSepolia, BaseGoerli } from '@particle-network/chains'
+import { evmWallets } from '@particle-network/connect'
 
 export default {
   projectId: process.env.NEXT_PUBLIC_PARTICLE_PROJECT_ID ?? '',
   clientKey: process.env.NEXT_PUBLIC_PARTICLE_CLIENT_KEY ?? '',
   appId: process.env.NEXT_PUBLIC_PARTICLE_APP_ID ?? '',
   chains: [
-    Solana
+    MantleTestnet, ScrollSepolia, BaseGoerli
   ],
   particleWalletEntry: {    //optional: particle wallet config
     displayWalletEntry: true, //display wallet button when connect particle success.
     defaultWalletEntryPosition: WalletEntryPosition.BR,
     supportChains: [
-      Solana
+      MantleTestnet, ScrollSepolia, BaseGoerli
     ],
     customStyle: {}, //optional: custom wallet style
   },
@@ -23,5 +23,7 @@ export default {
     //prompt set master password. 0: None(default), 1: Once, 2: Always
     promptMasterPasswordSettingWhenLogin: 0
   },
-  wallets: solanaWallets(),
+  wallets: evmWallets({
+    projectId: process.env.NEXT_PUBLIC_PARTICLE_PROJECT_ID ?? '',
+  }),
 }
